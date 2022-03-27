dbd37e5a579458300b516d3122c8b8f1c6921313
John <john.minns@missguided.com>
2022-03-27 15:41:24 +0100

    Add migration base

--- migrations/migration.go
+++ migrations/migration.go
@@ -1,4 +1,24 @@
 package migrations
 
+import (
+	"log"
+
+	"github.com/Johnogram/golang-todos/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
 func main() {
+	log.SetPrefix("db: ")
+	log.SetFlags(0)
+
+	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	db.AutoMigrate(&models.Task{})
 }
