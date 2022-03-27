957e653aaa27cfb857bd5486a36f92ae75d31d9e
John <john.minns@missguided.com>
2022-03-27 15:32:22 +0100

    Task model

--- models/task.go
+++ models/task.go
@@ -1,4 +1,15 @@
 package models
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Task struct {
+	ID        uuid.UUID `gorm:"primaryKey"`
+	Desc      string
+	Complete  bool
+	CreatedAt time.Time `gorm:"autoUpdateTime:nano"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime:nano"`
 }
